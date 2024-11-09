fbffa5ae9ae8010327a382a449fab145740f6fc4
toponima <toponima2024@gmail.com>
2024-11-09 14:04:09 -0500

    9112024

--- src/pages/explore/storydetail.jsx
+++ src/pages/explore/storydetail.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import { XMarkIcon, ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
+
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../../fb";       
 import { useParams } from 'react-router-dom';
@@ -6,6 +8,7 @@ import { useParams } from 'react-router-dom';
 import {
   Typography,
   Button,
+  IconButton,
 } from "@material-tailwind/react";
 import {  Link } from "react-router-dom";
 import ExpandableContent from './expandablecontent';
@@ -15,7 +18,20 @@ import LoadingSpinner from '@/widgets/loading/LoadingSpinner';
 function StoryDetail() {
     const { id } = useParams();
     const [story, setStory] = useState();
-
+    const [selectedImg, setSelectedImg] = useState(0);
+    // const [openDialog, setOpenDialog] = useState(false);
+    const [imgPop, setImgPop] = useState(false);
+    const swipeImg = (moveType) => {
+      if (moveType == "prv") {
+        if (selectedImg == 0) setSelectedImg(story?.imagesUrls.length - 1);
+        else setSelectedImg(selectedImg - 1);
+      }
+  
+      if (moveType == "nxt") {
+        if (selectedImg == story?.imagesUrls.length - 1) setSelectedImg(0);
+        else setSelectedImg(selectedImg + 1);
+      }
+    };
     
     const fetchStory = async ( id ) => { 
         try {
@@ -107,11 +123,58 @@ function StoryDetail() {
                   className="w-full rounded-lg object-cover object-center "
                   src={imageLink}
                   alt="imageLink"
+                  onClick={() => setImgPop(true)}
                 />
               </div>
             ))}
           </div>
-
+          {imgPop && (
+                    <div className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-60 z-50 flex justify-between items-center gap-3">
+                                    
+                                            <a
+            onClick={() => setImgPop(false)}
+            className="absolute top-2 right-2 cursor-pointer"
+          >
+              <IconButton
+                variant="text"
+                color="blue-gray"
+              >
+              <XMarkIcon strokeWidth={2.5} className="h-5 w-5" style={{color:'white', height:'20px', width:'20px', backgroundColor:'red', borderRadius:'50%', padding:'1px'}} />
+            </IconButton>
+            </a>
+          
+          <a
+            onClick={() => swipeImg("prv")}
+            className="bg-green rounded-full ml-2 hover:translate-x-2 transition-all ease-linear cursor-pointer"
+          >
+              <IconButton
+                variant="text"
+                color="blue-gray"
+              >
+              <ChevronLeftIcon strokeWidth={2.5} className="h-5 w-5" style={{color:'white', backgroundColor:'green', borderRadius:'50%', padding:'1px', height:'30px', width:'30px'}} />
+            </IconButton>
+            </a>
+            <div>
+                            <img
+                  className="w-full rounded-lg object-cover object-center "
+                  src={story?.imagesUrls[selectedImg]}
+                  alt="imageLink"
+                  onClick={() => setImgPop(true)}
+                />
+          </div>
+            <a
+            onClick={() => swipeImg("nxt")}
+            className="bg-green rounded-full mr-2 hover:-translate-x-2 transition-all ease-linear cursor-pointer"
+          >
+              <IconButton
+                variant="text"
+                color="blue-gray"
+              >
+              <ChevronRightIcon strokeWidth={2.5} className="h-5 w-5" style={{color:'white', backgroundColor:'green', borderRadius:'50%', padding:'1px', height:'30px', width:'30px'}} />
+            </IconButton>
+            </a>
+                    </div>
+          )}
     </div>
   )
 }
